bb769f4cc06c3e8d0bcacbd1ee1718c71fc872c5
Tobias Pihl <tobpi009@student.liu.se>
2015-12-04 14:07:41 +0100

    Separated graphics function into drawGraphics.js

--- game.js
+++ game.js
@@ -21,7 +21,8 @@ var tileSize = 10;
 var maze = [];
 var mazeWidth = 60;
 var mazeHeight = 40;
-var mazeGraphics;
+
+//var mazeGraphics;
 
 //easystar path
 var globalPath = null;
@@ -162,23 +163,25 @@ playGame.prototype = {
 	}
 }
 
-//Draw player, updating his position
-function drawPlayer(xPos, yPox, xPosNew, yPoxNew) {
-	mazeGraphics.beginFill(0x25DD00);
-	mazeGraphics.drawRect(playerXPos * tileSize, playerYPos * tileSize, tileSize, tileSize);
-}
+// //Draw player, updating his position
+// function drawPlayer(xPos, yPox, xPosNew, yPoxNew) {
+	// mazeGraphics.beginFill(0x25DD00);
+	// mazeGraphics.drawRect(playerXPos * tileSize, playerYPos * tileSize, tileSize, tileSize);
+// }
 
-//Draw player, updating his position
-function drawMino() {
-	mazeGraphics.beginFill(0xFFFF00);
-	mazeGraphics.drawRect(minoXPos * tileSize, minoYPos * tileSize, tileSize, tileSize);
-}
+// //Draw player, updating his position
+// function drawMino() {
+	// mazeGraphics.beginFill(0xFFFF00);
+	// mazeGraphics.drawRect(minoXPos * tileSize, minoYPos * tileSize, tileSize, tileSize);
+// }
 
-//when player moves, delete player icon from old position
-function eraseOldPlayerPos(xPos, yPos) {
-	mazeGraphics.beginFill(0x000000);
-	mazeGraphics.drawRect(xPos * tileSize, yPos * tileSize, tileSize, tileSize);
-}
+// //when player moves, delete player icon from old position
+// function eraseOldPlayerPos(xPos, yPos) {
+	// mazeGraphics.beginFill(0x000000);
+	// mazeGraphics.drawRect(xPos * tileSize, yPos * tileSize, tileSize, tileSize);
+// }
+
+
 //Move the mino enemy
 function moveMino() {
 	game.time.events.loop(Phaser.Timer.SECOND/15, function(){
@@ -286,30 +289,30 @@ function updateGraphics(easystar) {
 	drawMino();
 }
 
-//Draw a path showing the calculated path to target location
-function drawPath(path){
+// //Draw a path showing the calculated path to target location
+// function drawPath(path){
 
-	var i = 1;
-	while (i<path.length-1) {
-		mazeGraphics.beginFill(0xFF0000);
-		mazeGraphics.drawRect(path[i].x * tileSize + 3, path[i].y * tileSize + 3, tileSize - 6, tileSize - 6);
-		i++;
-		mazeGraphics.endFill();
-	}
-	globalPath = path;
+	// var i = 1;
+	// while (i<path.length-1) {
+		// mazeGraphics.beginFill(0xFF0000);
+		// mazeGraphics.drawRect(path[i].x * tileSize + 3, path[i].y * tileSize + 3, tileSize - 6, tileSize - 6);
+		// i++;
+		// mazeGraphics.endFill();
+	// }
+	// globalPath = path;
 
-}
+// }
 
-//Draw the maze based on the grid array
-function drawMaze(posX, posY){
-	mazeGraphics.clear();
-	mazeGraphics.beginFill(0x968551);
-	for(i = 0; i < mazeHeight; i ++){
-		for(j = 0; j < mazeWidth; j ++){
-			if( maze[i][j] == 1){
-				mazeGraphics.drawRect(j * tileSize, i * tileSize, tileSize, tileSize);
-			}
-		}
-	}
-	mazeGraphics.endFill();
-}
+// //Draw the maze based on the grid array
+// function drawMaze(posX, posY){
+	// mazeGraphics.clear();
+	// mazeGraphics.beginFill(0x968551);
+	// for(i = 0; i < mazeHeight; i ++){
+		// for(j = 0; j < mazeWidth; j ++){
+			// if( maze[i][j] == 1){
+				// mazeGraphics.drawRect(j * tileSize, i * tileSize, tileSize, tileSize);
+			// }
+		// }
+	// }
+	// mazeGraphics.endFill();
+// }
